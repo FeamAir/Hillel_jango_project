8ad06177f800a79dc7a40e3094bbc60283d281d8
FeamAir <maryking.textile@gmail.com>
2022-05-15 21:58:56 +0300

    add html(index,list,create,update(teachers and groups), add new view update)

--- groups/views.py
+++ groups/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Group
 from django.views.decorators.csrf import csrf_exempt
 from .forms import GroupCreateForm
-from .utils import qs2html
+from webargs.fields import Int, Str
+from webargs.djangoparser import use_args
 
 
-def list_groups(request):
+@use_args(
+    {
+        "language_groups": Str(required=False),
+        "univer_subject": Str(required=False),
+        "cnt_students": Int(required=False),
+    },
+    location="query"
+)
+def list_groups(request, args):
     gr = Group.objects.all()
-    html = qs2html(gr)
-    return HttpResponse(html)
+    for key, value in args.items():
+        gr = gr.filter(**{key: value})
+
+    return render(
+        request,
+        "groups/list.html",
+        {'title': "List of Groups", "groups": gr}
+    )
 
 
 @csrf_exempt
@@ -24,13 +39,27 @@ def create_groups(request):
 
             return HttpResponseRedirect('/groups/')
 
-    html_form = f"""
-        <form method="post">
-            <table>
-                {form.as_table()}
-            </table>
-            <input type="submit" value="Create">
-        </form> 
-        """
+    return render(
+        request,
+        "groups/create.html",
+        {'title': "Create new Groups", "form": form}
+    )
 
-    return HttpResponse(html_form)
+
+@csrf_exempt
+def update_groups(request, pk):
+    group = Group.objects.get(pk=pk)
+    if request.method == 'GET':
+        form = GroupCreateForm(instance=group)
+    else:
+        form = GroupCreateForm(request.POST, instance=group)
+        if form.is_valid():
+            form.save()
+
+            return HttpResponseRedirect('/groups/')
+
+    return render(
+        request,
+        "groups/update.html",
+        {'title': "Update Groups", "form": form}
+    )
